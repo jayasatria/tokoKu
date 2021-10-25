a51f70056ea2e0244bcc1f8da013294a1b67a867
lalu jaya satria permana <jayasatria9@gmail.com>
2021-10-25 10:57:57 +0800

    menampilkan nama product di cart
    sudah berhasil

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -6,16 +6,12 @@
                 <hr>
         <div class="row">
           @foreach ($image_name as $item)
-              
-         <?php 
-          $name[] = parse_str($item['image_product']);
-        dd($name);
-         ?>
+         
             <div class="col">
                 <div class="card" style="width: 18rem;">
                     <img class="card-img-top" src="storage/images/{{ $item->image_product }}.jpg" alt="Card image cap">
                     <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
+                      <h5 class="card-title"><?= trim($item['image_product'], "_gr");?> Gram</h5>
                       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                       <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
