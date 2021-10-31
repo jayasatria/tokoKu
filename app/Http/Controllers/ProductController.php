e1aa48d6a817ec4c2f385d5ae8002803e020d04e
lalu jaya satria permana <jayasatria9@gmail.com>
2021-10-31 09:45:45 +0800

    insert harga
    sudah bisa insert harga awal dan harga publish

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -47,24 +47,49 @@ class ProductController extends Controller
     public function store(Request $request)
     {
         $tanggal = $request->input('tanggal');
-        $oo25 = $request->input('0.025_gr');
-        $oo5 = $request->input('0.05_gr');
-        $o1 = $request->input('0.1_gr');
-        $o2 = $request->input('0.2_gr');
-        $o5 = $request->input('0.5_gr');
+        $oo25 = $request->input('0,025_gr');
+        $oo250 = round(((int)$oo25 * 0.12 + 3000) + (int)$oo25, -3);
+        $oo5 = $request->input('0,05_gr');
+        $oo50 = round(((int)$oo5 * 0.12 + 3000) + (int)$oo5, -3);
+        $o1 = $request->input('0,1_gr');
+        $o10 = round(((int)$o1 * 0.062 + 8000) + (int)$o1, -3);
+        $o2 = $request->input('0,2_gr');
+        $o20 = round(((int)$o2 * 0.062 + 8000) + (int)$o2, -3);
+        $o5 = $request->input('0,5_gr');
+        $o50 = round(((int)$o5 * 0.052 + 10000) + (int)$o5, -3);
         $i1 = $request->input('1_gr');
+        $i01 = round(((int)$i1 * 0.052 + 15000) + (int)$i1, -3);
         $i5 = $request->input('5_gr');
+        $i05 = round(((int)$i5 * 0.052 + 40000) + (int)$i5, -3);
         $i10 = $request->input('10_gr');
+        $i010 = round(((int)$i10 * 0.047 + 40000) + (int)$i10, -3);
         $i25 = $request->input('25_gr');
+        $i025 = round(((int)$i25 * 0.042 + 40000) + (int)$i25, -3);
         $i50 = $request->input('50_gr');
+        $i050 = round(((int)$i50 * 0.037 + 40000) + (int)$i50, -3);
         $i100 = $request->input('100_gr');
+        $i0100 = round(((int)$i100 * 0.032 + 40000) + (int)$i100, -3);
+        $request->validate([
+            'tanggal' => 'required',
+            '0,025_gr' => 'required',
+            '0,05_gr' => 'required',
+            '0,1_gr' => 'required',
+            '0,2_gr' => 'required',
+            '0,5_gr' => 'required',
+            '1_gr' => 'required',
+            '5_gr' => 'required',
+            '10_gr' => 'required',
+            '25_gr' => 'required',
+            '50_gr' => 'required',
+            '100_gr' => 'required'
+        ]);
         $data = [
             'tanggal' => $tanggal,
-            '0.025_gr' => $oo25,
-            '0.05_gr' => $oo5,
-            '0.1_gr' => $o1,
-            '0.2_gr' => $o2,
-            '0.5_gr' => $o5,
+            '0,025_gr' => $oo25,
+            '0,05_gr' => $oo5,
+            '0,1_gr' => $o1,
+            '0,2_gr' => $o2,
+            '0,5_gr' => $o5,
             '1_gr' => $i1,
             '5_gr' => $i5,
             '10_gr' => $i10,
@@ -72,8 +97,25 @@ class ProductController extends Controller
             '50_gr' => $i50,
             '100_gr' => $i100
         ];
-        DB::table('harga_awal')->insert($data);
-        return redirect()->route('/harga');
+        $data2 = [
+            'tanggal' => $tanggal,
+            '0,025_gr' => $oo250,
+            '0,05_gr' => $oo50,
+            '0,1_gr' => $o10,
+            '0,2_gr' => $o20,
+            '0,5_gr' => $o50,
+            '1_gr' => $i01,
+            '5_gr' => $i05,
+            '10_gr' => $i010,
+            '25_gr' => $i025,
+            '50_gr' => $i050,
+            '100_gr' => $i0100
+        ];
+        DB::table('products')->insert($data2);
+        $query = DB::table('harga_awal')->insert($data);
+        if ($query) {
+            return back();
+        }
     }
 
     /**
--- resources/views/harga/create.blade.php
+++ resources/views/harga/create.blade.php
@@ -5,60 +5,97 @@
         <div class="card my-4">
             <div class="card-body" >
                 <form class="form-group" action="/store" method="POST">
+                    @csrf
                     <div class="mb-3">
                         <label for="tanggal" class="form-label">Tanggal</label>
-                        <input type="date" class="form-control" id="tanggal">
+                        <input type="date" class="form-control" name="tanggal" id="tanggal" value="{{ old('tanggal') }}">
+                        <span style="color: red">@error('tanggal')
+                            {{ $message }}
+                        @enderror</span>
                     </div>
                     <div class="d-flex justify-content-lg-between">
                         <div class="mb-3">
-                            <label for="0.025_gr" class="form-label">0.025 gr</label>
-                            <input placeholder="Rp" type="number" min="0" max="100000000" class="form-control" id="0.025_gr">
+                            <label for="0,025_gr" class="form-label">0.025 gr</label>
+                            <input placeholder="Rp" type="number" class="form-control" name="0,025_gr" value="{{ old('0,025_gr') }}">
+                            <span style="color: red">@error('0,025_gr')
+                                {{ $message }}
+                            @enderror</span>
                         </div>
                         <div class="mb-3">
-                            <label for="0.05_gr" class="form-label">0.05 gr</label>
-                            <input placeholder="Rp" type="number" min="0" max="100000000" class="form-control" id="0.05_gr">
+                            <label for="0,05_gr" class="form-label">0.05 gr</label>
+                            <input placeholder="Rp" type="number" class="form-control" name="0,05_gr" value="{{ old('0,05_gr') }}">
+                            <span style="color: red">@error('0,05_gr')
+                                {{ $message }}
+                            @enderror</span>
                         </div>
                         <div class="mb-3">
-                            <label for="0.1_gr" class="form-label">0.1 gr</label>
-                            <input placeholder="Rp" type="number" min="0" max="100000000" class="form-control" id="0.1_gr">
+                            <label for="0,1_gr" class="form-label">0.1 gr</label>
+                            <input placeholder="Rp" type="number" class="form-control" name="0,1_gr" value="{{ old('0,1_gr') }}">
+                            <span style="color: red">@error('0,1_gr')
+                                {{ $message }}
+                            @enderror</span>
                         </div>
                     </div>
                     <div class="d-flex justify-content-lg-between">
                         <div class="mb-3">
-                            <label for="0.2_gr" class="form-label">0.2 gr</label>
-                            <input placeholder="Rp" type="number" min="0" max="100000000" class="form-control" id="0.2_gr">
+                            <label for="0,2_gr" class="form-label">0.2 gr</label>
+                            <input placeholder="Rp" type="number" class="form-control" name="0,2_gr" value="{{ old('0,2_gr') }}">
+                            <span style="color: red">@error('0,2_gr')
+                                {{ $message }}
+                            @enderror</span>
                         </div>
                         <div class="mb-3">
-                            <label for="0.5_gr" class="form-label">0.5 gr</label>
-                            <input placeholder="Rp" type="number" min="0" max="100000000" class="form-control" id="0.5_gr">
+                            <label for="0,5_gr" class="form-label">0.5 gr</label>
+                            <input placeholder="Rp" type="number" class="form-control" name="0,5_gr" value="{{ old('0,5_gr') }}">
+                            <span style="color: red">@error('0,5_gr')
+                                {{ $message }}
+                            @enderror</span>
                         </div>
                         <div class="mb-3">
                             <label for="1_gr" class="form-label">1 gr</label>
-                            <input placeholder="Rp" type="number" min="0" max="100000000" class="form-control" id="1_gr">
+                            <input placeholder="Rp" type="number" class="form-control" name="1_gr" value="{{ old('1_gr') }}">
+                            <span style="color: red">@error('1_gr')
+                                {{ $message }}
+                            @enderror</span>
                         </div>
                     </div>
                     <div class="d-flex justify-content-lg-between">
                         <div class="mb-3">
                             <label for="5_gr" class="form-label">5 gr</label>
-                            <input placeholder="Rp" type="number" min="0" max="100000000" class="form-control" id="5_gr">
+                            <input placeholder="Rp" type="number" class="form-control" name="5_gr" value="{{ old('5_gr') }}">
+                            <span style="color: red">@error('5_gr')
+                                {{ $message }}
+                            @enderror</span>
                         </div>
                         <div class="mb-3">
                             <label for="10_gr" class="form-label">10 gr</label>
-                            <input placeholder="Rp" type="number" min="0" max="100000000" class="form-control" id="10_gr">
+                            <input placeholder="Rp" type="number" class="form-control" name="10_gr" value="{{ old('10_gr') }}">
+                            <span style="color: red">@error('10_gr')
+                                {{ $message }}
+                            @enderror</span>
                         </div>
                         <div class="mb-3">
                             <label for="25_gr" class="form-label">25 gr</label>
-                            <input placeholder="Rp" type="number" min="0" max="100000000" class="form-control" id="25_gr">
+                            <input placeholder="Rp" type="number" class="form-control" name="25_gr" value="{{ old('25_gr') }}">
+                            <span style="color: red">@error('25_gr')
+                                {{ $message }}
+                            @enderror</span>
                         </div>
                     </div>
                     <div class="d-flex justify-content-lg-between">
                         <div class="mb-3">
                             <label for="50_gr" class="form-label">50 gr</label>
-                            <input placeholder="Rp" type="number" min="0" max="100000000" class="form-control" id="50_gr">
+                            <input placeholder="Rp" type="number" class="form-control" name="50_gr" value="{{ old('50_gr') }}">
+                            <span style="color: red">@error('50_gr')
+                                {{ $message }}
+                            @enderror</span>
                         </div>
                         <div class="mb-3">
                             <label for="100_gr" class="form-label">100 gr</label>
-                            <input placeholder="Rp" type="number" min="0" max="100000000" class="form-control" id="100_gr">
+                            <input placeholder="Rp" type="number" class="form-control" name="100_gr" value="{{ old('100_gr') }}">
+                            <span style="color: red">@error('100_gr')
+                                {{ $message }}
+                            @enderror</span>
                         </div>
                     </div>  
                     <button type="submit" class="btn btn-primary">Submit</button>
